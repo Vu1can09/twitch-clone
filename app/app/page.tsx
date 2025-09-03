92cbf817b1b1dfcdd2a696746ef90c8cc7e61d2e
Vu1can09 <yshvardhansingh0912@gmail.com>
2025-09-03 14:50:47 +0530

    first commit

---
+++ app/app/page.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import { useDatabase } from '@/contexts/databaseContext';
+import { useSession } from '@clerk/nextjs';
+import { SignedInSessionResource } from '@clerk/types';
+import { useEffect, useState } from 'react';
+import Onboarding from '@/components/onboarding/onboarding';
+import SelectInterests from '@/components/onboarding/selectInterests';
+import { Tables } from '@/database/database.types';
+import LiveChannels from '@/components/liveChannels/liveChannels';
+import HomeFeed from '@/components/homeFeed/homeFeed';
+
+export default function AppPage() {
+  const { session } = useSession();
+  const {
+    supabase,
+    setSupabaseClient,
+    getUserData,
+    getLiveStreams,
+    setLiveStreamsMockData,
+    removeLiveStreamsMockData,
+  } = useDatabase();
+  const [showOnboarding, setShowOnboarding] = useState(false);
+  const [showSelectInterests, setShowSelectInterests] = useState(false);
+  const [livestreams, setLivestreams] = useState<Tables<'livestreams'>[]>([]);
+
+  useEffect(() => {
+    async function initializeSupabase(session: SignedInSessionResource) {
+      const token = (await session?.getToken()) as string;
+      if (token) {
+        setSupabaseClient(token);
+      }
+    }
+
+    if (session && !supabase) {
+      initializeSupabase(session);
+    } else {
+      console.log('No clerk session');
+    }
+  }, [session, setSupabaseClient, supabase]);
+
+  useEffect(() => {
+    console.log('Supabase', supabase);
+    console.log('Session', session?.user.id);
+    if (supabase && session?.user.id) {
+      getUserData(session?.user.id).then((user) => {
+        if (user) {
+          console.log(user);
+          if (user.interests.length === 0) {
+            setShowOnboarding(false);
+            setShowSelectInterests(true);
+          } else {
+            setShowOnboarding(false);
+            setShowSelectInterests(false);
+            getLiveStreams().then((livestreams) => {
+              setLivestreams(livestreams);
+            });
+          }
+        } else {
+          setShowOnboarding(true);
+        }
+      });
+    }
+  }, [supabase, session?.user.id, getUserData, getLiveStreams]);
+
+  if (showOnboarding) {
+    return <Onboarding />;
+  }
+
+  if (showSelectInterests) {
+    return <SelectInterests />;
+  }
+
+  return (
+    <>
+      <section className='grid h-full grid-cols-[auto_1fr]'>
+        <LiveChannels livestreams={livestreams} />
+        <HomeFeed livestreams={livestreams} />
+      </section>
+      <button onClick={() => setLiveStreamsMockData()}>
+        Set Livestreams Mock Data
+      </button>
+      <button onClick={() => removeLiveStreamsMockData()}>
+        Remove Livestreams Mock Data
+      </button>
+    </>
+  );
+}
---
+++ contexts/databaseContext.tsx
@@ -0,0 +1,276 @@
+import { SupabaseClient,createClient } from "@supabase/supabase-js";
+import { Children, createContext, useContext, useCallback } from "react";
+import { useState } from "react";
+import type { Tables } from "../database/database.types";
+import { liveStreams } from "@/database/mockData";
+
+
+
+type DatabaseContextType = {
+    supabase: SupabaseClient | null;
+    error : string | null;
+    setSupabaseClient: (accessToken: string) => void;
+    getUserData: (
+    userId: string,
+    field?: string
+  ) => Promise<Tables<'users'> | null>;
+    setUserData: (
+    userName: string,
+    imageUrl: string,
+    mail: string,
+    dateOfBirth: string,
+    userId: string
+  ) => Promise<Tables<'users'> | null>;
+    setUserInterests: (
+    interests: string[],
+    userId: string
+  ) => Promise<Tables<'users'> | null>;
+  getLiveStreams: () => Promise<Tables<'livestreams'>[]>;
+  createLiveStream: (
+    name: string,
+    categories: string[],
+    userName: string,
+    profileImageUrl: string,
+  ) => Promise<Tables<'livestreams'> | null>;
+    deleteLiveStream: (userName: string) => Promise<boolean>;
+    setLiveStreamsMockData: () => void;
+    removeLiveStreamsMockData: () => void;
+};
+
+export const DatabaseContext = createContext<DatabaseContextType | null>(null);
+
+export const DatabaseProvider = ({
+    children,
+}:{
+    children: React.ReactNode;
+}) => {
+    const [supabase,setSupabase]= useState<SupabaseClient | null>(null);
+    const [error,setError]= useState<string | null>(null);
+
+    const setSupabaseClient = useCallback((accessToken: string):void => {
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+    if (!supabaseUrl || !supabaseAnonKey) {
+      throw new Error('Missing Supabase environment variables');
+    }
+    const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
+      accessToken: async () => accessToken,
+    });
+
+    setSupabase(supabaseClient);
+}, []);
+    const getUserData = useCallback(
+    async (
+      userId: string,
+      field: string = 'user_id'
+    ): Promise<Tables<'users'> | null> => {
+      console.log(
+        'Getting user data from supabase: ',
+        supabase,
+        'for userId: ',
+        userId
+      );
+      if (!supabase) {
+        return null;
+      }
+      try {
+        const { data, error } = await supabase
+          .from('users')
+          .select('*')
+          // Filter case insensitive
+          .ilike(field, `%${userId}%`)
+          .single();
+
+        console.log('User data: ', data);
+        if (error) {
+          console.error('Error getting user data', error);
+          setError(`Error getting user data: ${error.message}`);
+          return null;
+        }
+        return data;
+      } catch (error) {
+        console.error('Error getting user data', error);
+        return null;
+      }
+    },
+    [supabase]
+  );
+  const setUserData = useCallback(
+    async (
+      userName: string,
+      imageUrl: string,
+      mail: string,
+      dateOfBirth: string,
+      userId: string
+    ): Promise<Tables<'users'> | null> => {
+      if (!supabase) {
+        return null;
+      }
+      const { data, error } = await supabase
+        .from('users')
+        .insert({
+          user_name: userName,
+          image_url: imageUrl,
+          mail: mail,
+          date_of_birth: dateOfBirth,
+          user_id: userId,
+          following: [],
+          followers: [],
+          interests: [],
+        })
+        .select()
+        .single();
+      if (error) {
+        console.error('Error setting user data', error);
+        setError(`Error setting user data: ${error.message}`);
+        return null;
+      }
+      return data as Tables<'users'>;
+    },
+    [supabase]
+  );
+  const setUserInterests = useCallback(
+    async (
+      interests: string[],
+      userId: string
+    ): Promise<Tables<'users'> | null> => {
+      if (!supabase) {
+        return null;
+      }
+      const { data, error } = await supabase
+        .from('users')
+        .update({ interests: interests })
+        .eq('user_id', userId)
+        .select()
+        .single();
+      if (error) {
+        console.error('Error setting user interests', error);
+        setError(`Error setting user interests: ${error.message}`);
+        return null;
+      }
+      return data as Tables<'users'>;
+    },
+    [supabase]
+  );
+
+  const getLiveStreams = useCallback(async (): Promise<Tables<'livestreams'>[]> => {
+    if(!supabase){
+      return [];
+    }
+    const {data,error}= await supabase 
+    .from ('livestreams')
+    .select('*');
+    if(error){
+      console.log('Error getting live streams',error);
+    }
+    return data as Tables<'livestreams'>[];
+  },[supabase]);
+  const createLiveStream = useCallback(
+    async (
+      name: string,
+      categories: string[],
+      userName: string,
+      profileImageUrl: string
+    ): Promise<Tables<'livestreams'> | null> => {
+      if (!supabase) {
+        console.error('[createLiveStream] Supabase not initialized');
+        return null;
+      }
+      const { data, error } = await supabase
+        .from('livestreams')
+        .insert({
+          name: name,
+          categories: categories,
+          user_name: userName,
+          profile_image_url: profileImageUrl,
+        })
+        .select()
+        .single();
+      if (error) {
+        console.log('Error creating livestream', error);
+        setError(error.message);
+        return null;
+      }
+      return data as Tables<'livestreams'>;
+    },
+    [supabase]
+  );
+
+  const deleteLiveStream = useCallback(
+    async (userName: string): Promise<boolean> => {
+      if (!supabase) {
+        console.error('[deleteLivestream] Supabase not initialized');
+        return false;
+      }
+      const { error } = await supabase
+        .from('livestreams')
+        .delete()
+        .eq('user_name', userName);
+      if (error) {
+        console.log('Error deleting livestream', error);
+        setError(error.message);
+        return false;
+      }
+      return true;
+    },
+    [supabase]
+  );
+  const setLiveStreamsMockData = useCallback(async () => {
+    if (!supabase) {
+      return;
+    }
+    const { data, error } = await supabase
+      .from('livestreams')
+      .insert(liveStreams);
+    if (error) {
+      console.log('Error setting mock data', error);
+    }
+    return data;
+  }, [supabase]);
+
+  const removeLiveStreamsMockData = useCallback(async () => {
+    if (!supabase) {
+      return;
+    }
+    const { error } = await supabase
+      .from('livestreams')
+      .delete()
+      .in(
+        'id',
+        liveStreams.map((livestream) => livestream.id)
+      );
+    if (error) {
+      console.log('Error removing mock data', error);
+    }
+  }, [supabase]);
+
+
+
+    return (
+        <DatabaseContext.Provider value={{
+                supabase,
+        error,
+        setSupabaseClient,
+        getUserData,
+        setUserData,
+        setUserInterests,
+        getLiveStreams,
+        createLiveStream,
+        deleteLiveStream,
+        setLiveStreamsMockData,
+        removeLiveStreamsMockData,
+        }}
+        >
+            {children}
+        </DatabaseContext.Provider>
+    );
+};
+
+export const useDatabase = () => {
+    const context = useContext(DatabaseContext);
+    if (!context) {
+        throw new Error('useDatabase must be used within a DatabaseProvider');
+    }
+    return context; 
+};
